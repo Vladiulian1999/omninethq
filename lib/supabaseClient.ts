76d5a6f402eebe0c6ce83734dc5016089bb5d1f2
vladiulian1999 <vladiulia418@gmail.com>
2025-05-23 16:05:37 +0100

    Clean project structure and fix routing

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://tuysxhhlwsuehadbvrdi.supabase.co'
+const supabaseUrl = 'https://tuysxhhlswuehadbvrdj.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR1eXN4aGhsd3N1ZWhhZGJ2cmRpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc5MDY5ODQsImV4cCI6MjA2MzQ4Mjk4NH0.nU4PsFFQkSGX3HYRaffr4UAPENUbq25uGrc4MJ6MKdk'
-export const supabase = createClient(supabaseUrl, supabaseKey);
 
+export const supabase = createClient(supabaseUrl, supabaseKey);
