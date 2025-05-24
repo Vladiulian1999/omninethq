55823f4e0095d47bfe65dfe45053e61de978a6a9
vladiulian1999 <vladiulia418@gmail.com>
2025-05-24 17:04:06 +0100

    Remove static /tag/test route

--- tag/test/page.tsx
+++
@@ -1,9 +0,0 @@
-export const dynamic = 'force-dynamic';
-
-export default function TestTagPage() {
-  return (
-    <div className="p-6 text-center">
-      <h1 className="text-2xl font-bold">Static Tag Page: /tag/test</h1>
-    </div>
-  );
-}
